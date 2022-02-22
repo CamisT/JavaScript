d53ba4090273dc95ffecd8ca3c5d461ca02fb9d1
CamisT <87918920+CamisT@users.noreply.github.com>
2022-02-22 01:53:37 -0300

    Add files via upload

---
+++ Lista02-002.js
@@ -0,0 +1,13 @@
+const texto=prompt('Digite o texto para ler de diferentes formas')
+
+let arr=[]
+
+arr.push(texto)
+
+alert(texto.toUpperCase())
+
+alert(texto.toLowerCase())
+
+alert(texto.split(''))
+
+console.log(texto.split(''))
